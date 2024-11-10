bf7df6c190cfa9d899a15118165e095b61849eb5
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-11-10 09:20:53 +0200

    Define basic store structure with getState, subscribe, and dispatch methods

---
+++ src/store.js
@@ -0,0 +1,26 @@
+const store = {
+    state: { count: 0 },
+    getState() {
+        return this.state;
+    },
+    subscribers: [],
+    subscribe(callback) {
+        this.subscribers.push(callback);
+    },
+    dispatch(action) {
+        switch (action.type) {
+            case 'ADD':
+                this.state.count += 1;
+                break;
+            case 'SUBTRACT':
+                this.state.count -= 1;
+                break;
+            case 'RESET':
+                this.state.count = 0;
+                break;
+            default:
+                console.warn(`Unknown action: ${action.type}`);
+        }
+        this.subscribers.forEach(subscriber => subscriber());
+    }
+};
